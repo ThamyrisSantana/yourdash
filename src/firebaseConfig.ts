95bc738134e40ddf68ce48332eee04e27ef10b93
ThamyrisSantana <thamyrissatana3@gmail.com>
2022-08-14 18:58:31 -0300

    feat: add firebase config

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_qRnfhvhqQC4wNdp_ENGQqRRbnhp9JS4",
+  authDomain: "yourdash-d77ad.firebaseapp.com",
+  projectId: "yourdash-d77ad",
+  storageBucket: "yourdash-d77ad.appspot.com",
+  messagingSenderId: "427823879745",
+  appId: "1:427823879745:web:97301924fd1f2aae1ad8c0",
+};
+
+export const app = initializeApp(firebaseConfig);
