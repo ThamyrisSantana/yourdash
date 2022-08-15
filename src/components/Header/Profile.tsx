b5605d445a4a173a11664c22fed621b3df28cecf
ThamyrisSantana <thamyrissatana3@gmail.com>
2022-08-14 22:44:13 -0300

    feat: add dinamics user informations

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,26 +1,28 @@
-import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
-import React from "react";
+import { Avatar, Box, Button, Flex, Text } from "@chakra-ui/react";
+import React, { useContext } from "react";
+import { AuthGoogleContext } from "../../context/AuthGoogle";
 
 interface ProfileProps {
   showProfileData: boolean;
 }
 
 export function Profile({ showProfileData = true }: ProfileProps) {
+  const { user, signOut } = useContext(AuthGoogleContext);
+
   return (
     <Flex align="center">
       {showProfileData && (
         <Box mr="4" textAlign="right">
-          <Text>Name</Text>
+          <Text>{user.displayName}</Text>
           <Text color="gray.300" fontSize="small">
-            name@email.com
+            {user.email}
           </Text>
         </Box>
       )}
-      <Avatar
-        size="md"
-        name="Thamyris Sant Ana"
-        src="https://github.com/ThamyrisSantana.png"
-      />
+      <Avatar size="md" name={user.displayName} src={user.photoURL} />
+      <Button size="sm" ml="2" colorScheme="pink" onClick={() => signOut()}>
+        Sair
+      </Button>
     </Flex>
   );
 }
