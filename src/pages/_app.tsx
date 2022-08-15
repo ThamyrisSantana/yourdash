8432f0708dde496936450aa3cc7f34a802bb0f50
ThamyrisSantana <thamyrissatana3@gmail.com>
2022-08-14 22:45:07 -0300

    feat: add AuthGoogleProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import { SideBarDrawerProvider } from "../context/SideBarDrawerContext";
+import { AuthGoogleProvider } from "../context/AuthGoogle";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <SideBarDrawerProvider>
-        <Component {...pageProps} />
+        <AuthGoogleProvider>
+          <Component {...pageProps} />
+        </AuthGoogleProvider>
       </SideBarDrawerProvider>
     </ChakraProvider>
   );
