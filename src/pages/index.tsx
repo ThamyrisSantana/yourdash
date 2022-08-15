16e9be5e5af16d070e2c5caf01b0488cde0f4971
ThamyrisSantana <thamyrissatana3@gmail.com>
2022-08-14 22:42:10 -0300

    feat: add sign in with google button to sing in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
-import { Button, Flex, Stack, Text } from "@chakra-ui/react";
+import { Button, Flex, Icon, Stack, Text } from "@chakra-ui/react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/form/Input";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useContext } from "react";
+import { AuthGoogleContext } from "../context/AuthGoogle";
+import { FcGoogle } from "react-icons/fc";
 
 type SignInFormData = {
   email: string;
@@ -22,12 +25,23 @@ export function SignIn() {
     resolver: yupResolver(signInFormSchema),
   });
 
+  const { signInWithGoogle, signed } = useContext(AuthGoogleContext);
+
   const { errors } = formState;
 
   const handleSignIn: SubmitHandler<FieldValues> = async (values) => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
     console.log(values);
   };
+
+  if (signed) {
+    window.location.replace("/dashboard");
+  }
+
+  const googleAuth = async () => {
+    await signInWithGoogle();
+  };
+
   return (
     <Flex
       direction="column"
@@ -73,10 +87,20 @@ export function SignIn() {
           <Button
             type="submit"
             mt="6"
+            mb="6"
             colorScheme="pink"
             isLoading={formState.isSubmitting}
           >
             Entrar
+          </Button>
+
+          <Button
+            backgroundColor="#fff"
+            onClick={() => googleAuth()}
+            color="GrayText"
+          >
+            <Icon as={FcGoogle} size={60} mr="4" />
+            Entrar com Google
           </Button>
         </Flex>
       </Stack>
